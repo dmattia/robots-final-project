26468d1488cb58210f9df5ffd66ce2a846bf8066
dmattia <dmattia@nd.edu>
2016-03-23 22:40:56 -0400

    Connected to Firebase database

--- python_web_scraper/scripts/web_pub.py
+++ python_web_scraper/scripts/web_pub.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import rospy
+import requests
+import json
 from geometry_msgs.msg import Twist
 
 def web_pub():
@@ -8,12 +10,36 @@ def web_pub():
     rospy.init_node('web_pub', anonymous=True)
     rate = rospy.Rate(10) # 10hz
 
-    twist = Twist()
-    twist.linear.x = 0.3
-    twist.angular.z = 0.3
+    #twist = Twist()
+    #twist.linear.x = 0.3
+    #twist.angular.z = 0.3
+    players = {
+        'Blue Player': 0,
+	'Green Player': 0,
+	'Red Player': 0,
+	'Yellow Player': 0
+    }
 
     while not rospy.is_shutdown():
-        pub_twist.publish(twist)
+	r = requests.get('https://ros.firebaseio.com/.json')
+	data = r.json()
+	#rospy.loginfo("New Request with status: " + str(r.status_code))
+	update_made = False
+	if r.status_code == 200:
+	    # If any values in @data do not match values in @players,
+	    # then an update must have occurred
+	    for player_name in players:
+		player_score = data[player_name]
+		if player_score != players[player_name]:
+			#rospy.loginfo(str(player_score) + " does not equal " + str(players[player_name]))
+			update_made = True
+		players[player_name] = player_score
+	if update_made:
+            rospy.loginfo("Update made")
+	    for player_name in players:
+		rospy.loginfo("    " + player_name + ": " + str(players[player_name]))
+
+        #pub_twist.publish(twist)
         rate.sleep()
 
 if __name__ == '__main__':
